dd774fbe02440b7c9ac99c5ebbf853422c31f41a
Marton Csikos <marton.csikos@gmail.com>
2018-06-05 16:33:28 +0200

    add hidden option to set chance pct for 500 error in test server

--- testserver/index.js
+++ testserver/index.js
@@ -15,9 +15,11 @@ server.route({
   path: '/',
   async handler(request, h) {
     const timeout = request.query.wait || DEFAULT_TIMEOUT;
-    const statusCode = parseInt(request.query.statusCode, 10) || DEFAULT_STATUS_CODE;
-    const payload = { timeout, statusCode };
+    let statusCode = parseInt(request.query.statusCode, 10) || DEFAULT_STATUS_CODE;
 
+    if (request.query.error > Math.round(Math.random() * 100)) statusCode = 500;
+
+    const payload = { timeout, statusCode };
     const response = h.response(payload);
     response.code(statusCode);
     await wait(timeout);
