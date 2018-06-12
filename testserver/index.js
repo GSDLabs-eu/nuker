84f43f049b805bd1f2e927ba36f9954d042dacab
Marton Csikos <marton.csikos@gmail.com>
2018-06-12 16:08:04 +0200

    add variable timeout option to test server

--- testserver/index.js
+++ testserver/index.js
@@ -2,11 +2,13 @@ const http = require('http');
 const url = require('url');
 
 const DEFAULT_TIMEOUT = 0;
+const DEFAULT_VAR_TIMEOUT = 0;
 const DEFAULT_STATUS_CODE = 200;
 
 const requestHandler = async (req, res) => {
   const { query } = url.parse(req.url, true);
-  const timeout = query.wait || DEFAULT_TIMEOUT;
+  let timeout = parseInt(query.wait || DEFAULT_TIMEOUT, 10);
+  timeout += Math.round(Math.random() * parseInt(query.varWait || DEFAULT_VAR_TIMEOUT, 10));
   let statusCode = parseInt(query.statusCode, 10) || DEFAULT_STATUS_CODE;
 
   if (query.error > Math.round(Math.random() * 100)) statusCode = 500;
